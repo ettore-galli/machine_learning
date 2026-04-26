4d42c9f544cbbff3b3a3a3a792f2d2aba4bf2c1e
ettore-galli <ettore.galli@gmail.com>
2026-04-26 16:09:45 +0200

    Dummy performs step correctly

--- agents_langgraph/dummy_agent.py
+++ agents_langgraph/dummy_agent.py
@@ -1,11 +1,13 @@
+from typing import List
+
 from langchain_core.tools import tool
 
 from langchain.agents import create_agent
-from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage, ToolMessage
 
 
 # Tool annotation identifies a function as a tool automatically
-@tool
+@tool()
 def find_sum(x: int, y: int) -> int:
     # The docstring comment describes the capabilities of the function
     # It is used by the agent to discover the function's inputs, outputs and capabilities
@@ -16,7 +18,7 @@ def find_sum(x: int, y: int) -> int:
     return x + y
 
 
-@tool
+@tool()
 def find_product(x: int, y: int) -> int:
     """
     This function is used to multiply two numbers and return their product.
@@ -31,18 +33,51 @@ class DummyModel:
 
     def bind_tools(self, *args, **kwargs):
         print("\n===== BIND_TOOLS CALLED =====")
-        print(args)
-        print(kwargs)
+        # print(args)
+        # print(kwargs)
         # Deve restituire self per chaining
         return self
 
-    def invoke(self, *args, **kwargs):
+    def invoke(self, messages, *args, **kwargs):
         print("\n===== INVOKE CALLED =====")
-        print(args)
-        print(kwargs)
+        # print(args)
+        # print(kwargs)
 
-        # L'agent si aspetta un AIMessage come risposta
-        return AIMessage(content="(dummy response)")
+        # print(messages)
+
+        if any(m.type == "tool" for m in messages):
+            tool_message = [m for m in messages if m.type == "tool"][0]
+            return AIMessage(content=f"Risultato: {tool_message.content}")
+
+        if check_word_in_message("sum", messages):
+            return AIMessage(
+                content="",
+                tool_calls=[
+                    {"id": "find_sum", "name": "find_sum", "args": {"x": 2, "y": 3}}
+                ],
+            )
+
+        if check_word_in_message("product", messages):
+            return AIMessage(
+                content="",
+                tool_calls=[
+                    {
+                        "id": "find_product",
+                        "name": "find_product",
+                        "args": {"x": 2, "y": 3},
+                    }
+                ],
+            )
+
+        return AIMessage(content="don't know")
+
+
+def check_word_in_message(word: str, message: AIMessage) -> bool:
+    return any(word.lower() in block for block in retrieve_ai_messages(message=message))
+
+
+def retrieve_ai_messages(message: AIMessage) -> List[str]:
+    return [message.content for message in message]
 
 
 agent_tools = [find_sum, find_product]
@@ -68,10 +103,10 @@ def main():
     # Get the final answer
     print(f"Agent returned : {result['messages'][-1].content} \n")
 
-    print("Step by Step execution : ")
+    # print("Step by Step execution : ")
 
-    for message in result["messages"]:
-        print(message.pretty_repr())
+    # for message in result["messages"]:
+    #     print(message.pretty_repr())
 
 
 if __name__ == "__main__":
