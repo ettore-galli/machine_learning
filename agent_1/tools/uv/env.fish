ee05c30a6ad933b266312a59e692a5208cc665d0
ettore-galli <ettore.galli@gmail.com>
2026-04-28 22:14:41 +0200

    Basic uv management

--- agent_1/tools/uv/env.fish
+++
@@ -1,4 +0,0 @@
-if not contains "$HOME/Documents/SVILUPPO/machine_learning/agent_1/tools/uv" $PATH
-    # Prepending path in case a system-installed binary needs to be overridden
-    set -x PATH "$HOME/Documents/SVILUPPO/machine_learning/agent_1/tools/uv" $PATH
-end
