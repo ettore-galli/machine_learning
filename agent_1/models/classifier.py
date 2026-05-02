a28bf00f967ff43c37a9b27022256a4b6f5b7b33
ettore-galli <ettore.galli@gmail.com>
2026-05-02 18:59:34 +0200

    draft training

--- agent_1/models/classifier.py
+++ agent_1/models/classifier.py
@@ -1,5 +1,8 @@
 import torch
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from transformers import (
+    DistilBertForSequenceClassification,
+    DistilBertTokenizer,
+)
 
 from models.general_llm_base import GeneralLLMBase
 
@@ -9,10 +12,10 @@ class ClassifierLLM(GeneralLLMBase):
         super().__init__("distilbert-base-uncased-finetuned-sst-2-english", logger)
 
     def do_perform(self, prompt: str, **kwargs) -> str:
-        tokenizer = AutoTokenizer.from_pretrained(self.model_id)
-        model = AutoModelForSequenceClassification.from_pretrained(
+        tokenizer = DistilBertTokenizer.from_pretrained(self.model_id)
+        model = DistilBertForSequenceClassification.from_pretrained(
             self.model_id,
-            torch_dtype=torch.float16 if self.device == "cuda" else torch.float32,
+            dtype=torch.float16 if self.device == "cuda" else torch.float32,
             device_map="auto" if self.device == "cuda" else None,
         )
 
@@ -23,8 +26,9 @@ class ClassifierLLM(GeneralLLMBase):
 
         generation_params = {**dict(num_beams=4), **kwargs}
 
-        outputs = model(**inputs, **generation_params)
-        predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
+        with torch.no_grad():
+            outputs = model(**inputs, **generation_params)
+            predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
         return {
             label: predictions[0][result_id].item()
             for result_id, label in model.config.id2label.items()
