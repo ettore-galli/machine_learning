790b42cd2f44b2ac5f2f9ca7683741bee7cda8ae
ettore-galli <ettore.galli@gmail.com>
2026-05-03 21:17:44 +0200

    move files

---
+++ agents_langgraph/demo_training.py
@@ -0,0 +1,35 @@
+import logging
+from typing import Any
+
+from datasets import load_dataset  # pyright: ignore[reportMissingTypeStubs]
+from transformers import AutoTokenizer
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+def demo():
+    raw_datasets = load_dataset("glue", "mrpc")
+
+    checkpoint: str = "bert-base-uncased"
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+    # tokenized_sentences_1 = tokenizer(raw_datasets["train"]["sentence1"])
+    # tokenized_sentences_2 = tokenizer(raw_datasets["train"]["sentence2"])
+
+    def tokenize_function(batch: dict[str, list[Any]]):
+        print("TOKENIZE batch", type(batch))
+        return tokenizer(
+            batch["sentence1"],
+            batch["sentence2"],
+            padding="max_length",
+            truncation=True,
+        )
+
+    tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+
+    _ = tokenizer.convert_ids_to_tokens(tokenized_datasets["train"][1]["input_ids"])
+
+    print(".")
+
+
+if __name__ == "__main__":
+    demo()
---
+++ agents_langgraph/run_training.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import demo_training as demo_training  # noqa: E402 F401
+
+demo_training.demo()
