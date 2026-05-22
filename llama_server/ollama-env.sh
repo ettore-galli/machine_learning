f641a5641d31cea76bbb7f309fa4013e46700d2a
ettore-galli <ettore.galli@gmail.com>
2026-05-22 07:25:07 +0200

    ollama mistral

--- llama_server/ollama-env.sh
+++ llama_server/ollama-env.sh
@@ -7,6 +7,7 @@ export EXECUTABLE="${OLLAMA_PATH}/Ollama.app/Contents/Resources/ollama"
 export MODEL_PATH="/Volumes/DOCKER/huggingface/downloaded/qwen2.5-3b-instruct-q8_0.gguf"
 
 export OLLAMA_MODEL_NAME="qwen-custom-1"
+export OLLAMA_MISTRAL_MODEL_NAME="mistral"
 
  
 
