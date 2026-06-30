dfd44df699952af23ce3f5f47a369dc8653a5622
ettore-galli <ettore.galli@gmail.com>
2026-06-30 08:13:47 +0200

    IMprove interaction

--- agent_5/ui/main.py
+++ agent_5/ui/main.py
@@ -1,12 +1,20 @@
 from ai_agent.base import build_agent_input, extract_response_message
 from ai_agent.agent_openai import initialize_agent
+from langchain.agents.middleware.types import InputAgentState
+from langgraph.graph.state import CompiledStateGraph
+
+
+def perform_model_interaction(agent: CompiledStateGraph, user_prompt: str) -> None:
+    agent_input: InputAgentState = build_agent_input(initial_user_prompt=user_prompt)
+    response = agent.invoke(agent_input)
+    print(extract_response_message(response))
 
 
 def main():
     print("Mini-Agent Locale (LangGraph + Llama.cpp)")
     print("Scrivi 'exit' per uscire.\n")
 
-    agent = initialize_agent()
+    agent: CompiledStateGraph = initialize_agent()
 
     while True:
         user_prompt = input("<prompt>: ")
@@ -14,9 +22,7 @@ def main():
         if user_prompt == "exit":
             break
 
-        response = agent.invoke(build_agent_input(initial_user_prompt=user_prompt))
-
-        print(extract_response_message(response))
+        perform_model_interaction(agent=agent, user_prompt=user_prompt)
 
 
 if __name__ == "__main__":
