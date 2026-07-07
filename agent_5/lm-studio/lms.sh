06d16eb20698bc14f31c77f718d697f2ed86afc4
ettore-galli <ettore.galli@gmail.com>
2026-07-07 14:32:47 +0200

    good setup befole plit add

--- agent_5/lm-studio/lms.sh
+++ agent_5/lm-studio/lms.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-SERVER_PORT="9876"
-SERVER_HOST="localhost"
-MISTRAL7B_LMS_MODEL_NAME=""huggingface/downloaded/mistral-7b-q4_k_m.gguf""
+export SERVER_PORT="9876"
+export SERVER_HOST="localhost"
+export MISTRAL7B_LMS_MODEL_NAME="huggingface/downloaded/mistral-7b-q4_k_m.gguf"
 
-lms load "${MODEL_MISTRAL7B_LMS_MODEL_NAME}" --gpu max -y   # sostituisci con il tuo modello
+lms load "${MISTRAL7B_LMS_MODEL_NAME}" --gpu max -y --context-length 8192  
 lms server start --port "${SERVER_PORT}" --cors --bind "${SERVER_HOST}"
