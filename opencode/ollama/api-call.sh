3d2c649480269ec07f2607856711d343e0130883
ettore-galli <ettore.galli@gmail.com>
2026-08-01 21:50:31 +0200

    params foe code

--- opencode/ollama/api-call.sh
+++ opencode/ollama/api-call.sh
@@ -4,11 +4,21 @@ source ./env.sh
 
 #!/bin/bash
 
+# endpoint="http://localhost:11434/api/generate";
+endpoint="http://localhost:11434/v1/completions";
+
 json=$(jq -n \
   --arg prompt "$1" \
-  '{model:"Qwen2.5-Coder", prompt:$prompt, stream:false}')
+  '{
+  model: "Qwen2.5-Coder", 
+  prompt: $prompt, 
+  stream: false,
+  max_tokens: 512
+  }')
 
-curl http://localhost:11434/api/generate \
+echo $json
+
+curl "${endpoint}" \
   -H "Content-Type: application/json" \
   -d "$json"
  
