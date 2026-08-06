4e9f46d8f9b0dba242d402b90489c64c3065083c
ettore-galli <ettore.galli@gmail.com>
2026-08-06 16:37:08 +0200

    Basic classifier

--- agent_8/tests/unit/ai_agent/test_tool_scan_folder.py
+++ agent_8/tests/unit/ai_agent/test_tool_scan_folder.py
@@ -4,6 +4,7 @@ from ai_agent.tools import scan_folder
 
 
 def test_scan_folder():
+    print(scan_folder(os.path.dirname(__file__)))
     assert sorted(
         [item["name"] for item in scan_folder(os.path.dirname(__file__))]
     ) == sorted(
--- agent_8/ui/main.py
+++ agent_8/ui/main.py
@@ -1,35 +1,31 @@
+from pathlib import Path
+
 from ai_agent.agent_proxy import get_model_response
+from ai_agent.tools import extract_text, scan_folder
 
-TOOLING_SYSTEM_PROMPT = """
-    Sei un assistente utile. Hai a disposizione i seguenti strumenti:
+CLASSIFY_SYSTEM_PROMPT = """
+Sei un classificatore di documenti.
+Dato il seguente testo, restituisci una ed una sola parola che rappresenta il tema principale trattato
+- UNA SOLA PAROLA
+- NON giustificare la scelta, fornisci solo il risultato
 
-    1. get_weather(city): per ottenere il meteo.
-    2. calculate_sum(a, b): per sommare due numeri.
-    3. calculate_product(a, b): per moltiplicare due numeri.
-    4. eval_expression(expr): per valutare un'espressione numerica più complessa
-    2. get_dir_list(root): per ottenere la lista di directory
-
-
-    Se l'utente chiede il meteo o una somma, rispondi nel seguente formato:
-
-    ACTION: nome_funzione, parametro1, parametro2, ecc ecc
-
-    Ad esempio
-
-    ACTION: get_weather, "Mulazzano"
-    ACTION: calculate_sum, 17, 34
-
-    Se l'utente fa una richiesta diversa, rispondi normalmente ignorando i tool.
+Testo:
     """
 
 GENERAL_SYSTEM_PROMPT = """
 Sei un assistente generico. Rispondi alle domande in modo preciso e conciso"""
+
 EXIT_WORD = "/bye"
+
 USER_PROMPT_MARKER: str = "Utente"
 
 
 def display_response_segment(segment: str) -> None:
     print(segment, end="", flush=True)
+
+
+def empty(*args, **kwargs) -> None:
+    _ = args, kwargs
 
 
 def perform_model_interaction(user_prompt: str) -> None:
@@ -41,17 +37,42 @@ def perform_model_interaction(user_prompt: str) -> None:
     )
 
 
+def classify_file_content(content: str) -> str:
+    return get_model_response(
+        user_prompt=content,
+        system_prompt=CLASSIFY_SYSTEM_PROMPT,
+        user_prompt_marker="Testo:",
+        response_displayer=empty,
+    )
+
+
 def main():
 
     print(f"Scrivi '{EXIT_WORD}' per uscire.\n")
 
+    default = "/users/ettoregalli/Downloads"
+
     while True:
-        user_prompt = input("---> ")
+        user_prompt = input(f"Cartella da classificare: [{default}]")
 
         if user_prompt == EXIT_WORD:
             break
 
-        perform_model_interaction(user_prompt=user_prompt)
+        user_prompt = user_prompt or default
+
+        print(f"Reading files in {user_prompt}")
+
+        for item in scan_folder(user_prompt):
+            file = item["path"]
+            try:
+                content = extract_text(path=Path(file))
+                if content:
+                    classification = classify_file_content(content[:1000])
+                    print(f"----- {file}, {classification}")
+            except UnicodeDecodeError as unicode_error:
+                print(f"----- ERR: {file}, {unicode_error}")
+            except Exception as other:  # noqa: BLE001
+                print(f"----- ERR: {file}, {other}")
 
 
 if __name__ == "__main__":
