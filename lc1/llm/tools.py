c169b5e8eb111037d950fbceb5fdaba5bd9fb5b4
ettore-galli <ettore.galli@gmail.com>
2026-08-13 11:37:25 +0200

    tool works

--- lc1/llm/tools.py
+++ lc1/llm/tools.py
@@ -4,10 +4,10 @@ from langchain.tools import tool
 
 
 @tool
-def get_downloads_directory() -> dict[str, str]:
+def get_downloads_directory() -> list[str]:
     """Get the directory listing of the Downloads directory"""
     print("STO USANDO IL TOOL get_downloads_directory()")
-    return {"path": os.path.expanduser("~/Downloads")}
+    return os.listdir("/Users/ettoregalli/Downloads")[:10] + ["altro..."]
 
 
 @tool
